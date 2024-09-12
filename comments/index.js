732f7afc78e39c562620a4036281b1f236d35b39
AngkonKumarRoy <angkon.kumar@bevycommerce.com>
2024-09-12 15:45:21 +0600

    feat: creating posts and lists and rendering it.

--- comments/index.js
+++ comments/index.js
@@ -1,28 +1,31 @@
 const express = require("express");
 const { randomBytes } = require("crypto");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
-const posts = {};
+const commentsByPostId = {};
 
-app.get("/posts", (req, res) => {
-  res.send(posts);
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts", (req, res) => {
-  const id = randomBytes(4).toString("hex");
-  const { title } = req.body;
+app.post("/posts/:id/comments", (req, res) => {
+  const commentId = randomBytes(4).toString("hex");
+  const { content } = req.body;
 
-  posts[id] = {
-    id,
-    title,
-  };
+  const comments = commentsByPostId[req.params.id] || [];
 
-  res.status(201).send(posts[id]);
+  comments.push({ id: commentId, content });
+
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).send(comments);
 });
 
-app.listen(4000, () => {
-  console.log("Listening on 4000");
+app.listen(4001, () => {
+  console.log("Listening on 4001");
 });
